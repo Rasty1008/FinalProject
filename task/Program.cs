61c708ee781d448bd43c2eecd764b4b6774d8546
Rasty <teterukrostislav@gmail.com>
2022-11-15 10:39:40 +0600

    add task

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Clear();
+string[] firstArray = new string[5] {"Hello", "-2", ":-)", "step", "43"};
+string[] secArray = new string[firstArray.Length];
+
+ResultArray(firstArray, secArray);
+PrintArray(secArray);
+
+void ResultArray(string[] array1, string[] array2) {
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++) {
+        if(array1[i].Length <= 3) {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
